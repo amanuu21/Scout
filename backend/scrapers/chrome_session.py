bb10e9ac4d5ead55bded41051067ac25262deaff
Amanuel Wondimu <amanuelabb@gmail.com>
2026-08-16 18:15:10 +0300

    chore: optimize memory usage during multi-tab headless Chrome sessions (iter 4)

--- backend/scrapers/chrome_session.py
+++ backend/scrapers/chrome_session.py
@@ -7,3 +7,6 @@
 
 # Progress update on 2026-08-10 - chore: optimize memory usage during multi-tab headless Chrome sessions (iter 3)
 # Revision: 30
+
+# Progress update on 2026-08-16 - chore: optimize memory usage during multi-tab headless Chrome sessions (iter 4)
+# Revision: 40
